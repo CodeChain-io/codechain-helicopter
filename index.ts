27ba035a57100c5c3ef749a0b3f026b83c05f144
Seulgi Kim <seulgi.kim@kodebox.io>
2018-09-18 15:14:17 +0900

    Extract util functions

--- index.ts
+++ index.ts
@@ -1,28 +1,14 @@
 import { BigNumber } from "bignumber.js";
 import { SDK } from "codechain-sdk";
 import { Parcel } from "codechain-sdk/lib/core/Parcel";
-import { U256 } from "codechain-sdk/lib/core/U256";
-import { KeyStore } from "codechain-sdk/lib/key/KeyStore";
 import * as config from "config";
 import * as request from "request-promise-native";
 import * as sleep from "sleep";
+import { calculateNonce, sendParcel } from "./util";
 
 interface Account {
     address: string;
     balance: BigNumber;
-}
-
-async function calculateNonce(sdk: SDK, payer: string): Promise<U256> {
-    const prevNonce = await sdk.rpc.chain.getNonce(payer);
-    const pendingParcels = await sdk.rpc.chain.getPendingParcels();
-    const payerParcels = pendingParcels.filter(
-        parcel => parcel.getSignerAddress().value === payer
-    );
-
-    if (payerParcels.length === 0) {
-        return await sdk.rpc.chain.getNonce(payer);
-    }
-    return new U256(prevNonce.value.plus(payerParcels.length));
 }
 
 function getRandomAccount(accounts: Account[]): string {
@@ -66,25 +52,6 @@ async function chooseAccount(
             excludedAccountList.indexOf(account.address) === -1
     );
     return getRandomAccount(accounts);
-}
-
-async function sendParcel(
-    sdk: SDK,
-    account: string,
-    passphrase: string,
-    keyStore: KeyStore,
-    nonce: U256,
-    parcel: Parcel
-): Promise<U256> {
-    const signedParcel = await sdk.key.signParcel(parcel, {
-        account,
-        keyStore,
-        fee: 10,
-        nonce,
-        passphrase
-    });
-    await sdk.rpc.chain.sendSignedParcel(signedParcel);
-    return nonce.increase();
 }
 
 async function airdropCCCParcel(
---
+++ util.ts
@@ -0,0 +1,36 @@
+import { SDK } from "codechain-sdk";
+import { Parcel } from "codechain-sdk/lib/core/Parcel";
+import { U256 } from "codechain-sdk/lib/core/U256";
+import { KeyStore } from "codechain-sdk/lib/key/KeyStore";
+
+export async function calculateNonce(sdk: SDK, payer: string): Promise<U256> {
+    const prevNonce = await sdk.rpc.chain.getNonce(payer);
+    const pendingParcels = await sdk.rpc.chain.getPendingParcels();
+    const payerParcels = pendingParcels.filter(
+        parcel => parcel.getSignerAddress().value === payer
+    );
+
+    if (payerParcels.length === 0) {
+        return await sdk.rpc.chain.getNonce(payer);
+    }
+    return new U256(prevNonce.value.plus(payerParcels.length));
+}
+
+export async function sendParcel(
+    sdk: SDK,
+    account: string,
+    passphrase: string,
+    keyStore: KeyStore,
+    nonce: U256,
+    parcel: Parcel
+): Promise<U256> {
+    const signedParcel = await sdk.key.signParcel(parcel, {
+        account,
+        keyStore,
+        fee: 10,
+        nonce,
+        passphrase
+    });
+    await sdk.rpc.chain.sendSignedParcel(signedParcel);
+    return nonce.increase();
+}
