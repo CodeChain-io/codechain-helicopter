797700196cb79ab73d607c128047a946566e0416
Seulgi Kim <seulgi.kim@kodebox.io>
2018-09-21 10:36:22 +0900

    A configuration can be boolean

--- util.ts
+++ util.ts
@@ -38,7 +38,7 @@ export async function sendParcel(
 
 export function getConfig<T>(field: string): T {
     const c = config.get<T>(field);
-    if (!c) {
+    if (c == null) {
         throw new Error(`${field} is not specified`);
     }
     return c;
