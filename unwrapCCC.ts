c888b9e5bb2bc8831bfa905bf1f3bd2dbe54268c
Seulgi Kim <seulgi.kim@kodebox.io>
2019-03-06 11:20:09 +0900

    Upgrade sdk
    It adds a receiver field to UnwrapCCC.

--- unwrapCCC.ts
+++ unwrapCCC.ts
@@ -4,10 +4,12 @@ import { UnwrapCCC, WrapCCC } from "codechain-sdk/lib/core/classes";
 export async function unwrapCCCTransaction(
     sdk: SDK,
     prevWrapped: WrapCCC,
+    receiver: string,
     networkId: string
 ): Promise<UnwrapCCC> {
     const unwrapCCCTx = sdk.core.createUnwrapCCCTransaction({
         burn: prevWrapped.getAsset(),
+        receiver,
         networkId
     });
     await sdk.key.signTransactionBurn(unwrapCCCTx, 0);
